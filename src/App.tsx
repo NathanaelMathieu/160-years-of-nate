cbdc972675da876a821b1fd782ed1aace82311bb
Nathanael Mathieu <nmathieu@wesleyan.edu>
2022-05-08 13:50:19 -0400

    update text

--- src/App.tsx
+++ src/App.tsx
@@ -14,22 +14,25 @@ function App() {
   const updateWidth = () => setWidth(window.innerWidth);
 
   React.useEffect(() => {
-    window.addEventListener("resize", updateWidth);
-    return () => window.removeEventListener("resize", updateWidth);
+    window.addEventListener('resize', updateWidth);
+    return () => window.removeEventListener('resize', updateWidth);
   });
 
   return (
-    <div className="App">
-     <ImageList variant="woven" gap={10} cols={width < 500 ? 1 : (width < 1000 ? 2 : 3)}>
+    <div className='App'>
+      <div className='Header'>
+        Most people have their quarter life crisis between the ages of 20 and 30 years old. Bucking that trend, Nate has decided to delay his quarter life crisis until he is 40 years old. This effectively increases his lifespan to 160 years. In celebration of his 24th/160 years, or 3/20th life party, Nate would like to invite you to 91 Rogers Ave, Somerville MA on May 13th from 8:00p - 11:00p. You are encouraged to come as your favorite year from the 160 Years of Nate. See below for outfit ideas.
+      </div>
+     <ImageList variant='woven' gap={10} cols={width < 500 ? 1 : (width < 1000 ? 2 : 3)}>
       {images.map((image) => {
-        const title = image.replace(/([a-z]?\..*\..*)|(\/.*\/)/gm,"").replace(/-00|-0|-/gm," ");
+        const title = image.replace(/([a-z]?\..*\..*)|(\/.*\/)/gm,'').replace(/-00|-0|-/gm,' ');
         return (
           <ImageListItem key={image}>
             <img
               src={`${image}?fit=crop&auto=format`}
               srcSet={`${image}?fit=crop&auto=format&dpr=2 2x`}
               alt={title}
-              loading="lazy"
+              loading='lazy'
             />
             <ImageListItemBar
               title={title}
