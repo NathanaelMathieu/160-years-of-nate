e87e36352ee7856a5e833953798ac7a3e3d4075a
Nathanael Mathieu <nmathieu@wesleyan.edu>
2022-05-08 12:23:40 -0400

    Responsive scaling

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,32 @@ function importAll(r: __WebpackModuleApi.RequireContext): string[] {
 const images: string[] = importAll(require.context('./images', false, /\.(png|jpe?g|svg|heic|JPE?G)$/));
 
 function App() {
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const updateWidth = () => setWidth(window.innerWidth);
+
+  React.useEffect(() => {
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
+  });
+
   return (
     <div className="App">
-     <ImageList variant="woven" gap={10}>
-      {images.map((image) => (
-        <ImageListItem key={image}>
-          <img
-            src={`${image}?fit=crop&auto=format`}
-            srcSet={`${image}?fit=crop&auto=format&dpr=2 2x`}
-            alt="image"
-            loading="lazy"
-          />
-          <ImageListItemBar
-            // position="below"
-            title={image.replace(/([a-z]?\..*\..*)|(\/.*\/)/gm,"").replace(/-0|-/gm," ")}
-          />
-        </ImageListItem>
-      ))}
+     <ImageList variant="woven" gap={10} cols={width < 500 ? 1 : (width < 1000 ? 2 : 3)}>
+      {images.map((image) => {
+        const title = image.replace(/([a-z]?\..*\..*)|(\/.*\/)/gm,"").replace(/-00|-0|-/gm," ");
+        return (
+          <ImageListItem key={image}>
+            <img
+              src={`${image}?fit=crop&auto=format`}
+              srcSet={`${image}?fit=crop&auto=format&dpr=2 2x`}
+              alt={title}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              title={title}
+            />
+          </ImageListItem>
+      )})}
      </ImageList>
     </div>
   );
